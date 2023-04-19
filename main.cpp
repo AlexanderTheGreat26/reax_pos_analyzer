a8e99da6c8a2aec1cc42122f6a860595ac7fcd29
alexander <kaegod@yandex.ru>
2023-04-19 12:50:38 +0300

    Eigen+

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 
 void read (const std::string & name, const std::string & cation, std::vector<int> & cations_count, int cations_per_mol);
 
-std::vector<int> sum (std::vector<int> & first, std::vector<int> & second);
+std::vector<int> sum (std::vector<int> & first, std::vector<int> & second, std::vector<int> & third);
 
 void data_file_creation (const std::string & name, std::vector<int> & cations);
 
@@ -19,21 +19,23 @@ int main() {
 
     std::string file_name = "CoMs.pos";
 
-    std::vector<int> Z, H30, H6O2, HO, H;
+    std::vector<int> Z, H30, H6O2, H9O4, OH, H;
 
     read(file_name, "H5O2", Z, 1);
     read(file_name, "H3O", H30, 1);
     read(file_name, "H6O2", H6O2, 2);
+    read(file_name, "H9O4", H9O4, 1);
 
-    read(file_name, "HO", HO, 1);
+
+    read(file_name, "HO", OH, 1);
     read(file_name, "\tH\t", H, 1);
 
-    H30 = std::move(sum(H30, H6O2));
+    H30 = std::move(sum(H30, H6O2, H9O4));
 
     data_file_creation("Zundels", Z);
     data_file_creation("H3O", H30);
 
-    data_file_creation("HO", HO);
+    data_file_creation("HO", OH);
     data_file_creation("free_protons", H);
 
     return 0;
@@ -57,8 +59,14 @@ void data_file_creation (const std::string & name, std::vector<int> & cations) {
 }
 
 
-std::vector<int> sum (std::vector<int> & first, std::vector<int> & second) {
-    int size = std::min(first.size(), second.size());
+unsigned long min (unsigned long a, unsigned long b, unsigned long c) {
+    unsigned long tmp = std::min(a, b);
+    return (tmp > c) ? c : tmp;
+}
+
+
+std::vector<int> sum (std::vector<int> & first, std::vector<int> & second, std::vector<int> & third) {
+    int size = min(first.size(), second.size(), third.size());
     std::vector<int> result (size);
     for (int i = 0; i < size; ++i)
         result[i] = first[i] + second[i];
